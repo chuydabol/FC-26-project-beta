f6ec87f7946b2c8fe095eeb6ccb10f81b429953e
chuydabol <chuylealrosa1234@gmail.com>
2025-08-20 14:36:37 -0700

    feat: migrate from firebase to postgres

--- test/eaMembers.test.js
+++ test/eaMembers.test.js
@@ -1,35 +1,10 @@
 const { test, mock } = require('node:test');
 const assert = require('assert');
 
-process.env.FIREBASE_SERVICE_ACCOUNT = JSON.stringify({
-  project_id: 'test',
-  client_email: 'test@test',
-  private_key: 'key'
-});
 process.env.DATABASE_URL = 'postgres://user:pass@localhost:5432/db';
-
-function firestore() { return {}; }
-firestore.FieldValue = {};
-firestore.FieldPath = {};
-
-const Module = require('module');
-const originalRequire = Module.prototype.require;
-Module.prototype.require = function (id) {
-  if (id === 'firebase-admin') {
-    return {
-      credential: { cert: svc => svc },
-      initializeApp: () => {},
-      apps: [],
-      app: () => ({ options: { projectId: 'test' } }),
-      firestore
-    };
-  }
-  return originalRequire.apply(this, arguments);
-};
 
 const eaApi = require('../services/eaApi');
 const app = require('../server');
-Module.prototype.require = originalRequire;
 
 async function withServer(fn) {
   const server = app.listen(0);
